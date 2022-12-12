b3f2cf8942297f22d1591d9c7948290635c352e4
saka108d <saka108d@msx.tu-dresden.de>
2022-12-12 18:48:49 +0100

    hello from pranavi

--- playgroung/views.py
+++ playgroung/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    #pull data from db
-    #transform
-    #send emails
     return HttpResponse('Hello from Pranavi.')
+    #return render(request, 'hello.html')
